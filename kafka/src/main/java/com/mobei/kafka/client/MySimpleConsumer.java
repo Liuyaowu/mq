5d18efa1bffd4c84951171d7b2426ad03ac9cd1e
mobei <mobei615@foxmail.com>
2021-11-21 16:48:03 +0800

    Kafka消费者的手动异步提交

--- kafka/src/main/java/com/mobei/kafka/client/MySimpleConsumer.java
+++ kafka/src/main/java/com/mobei/kafka/client/MySimpleConsumer.java
@@ -1,14 +1,13 @@
 package com.mobei.kafka.client;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
 import java.time.Duration;
 import java.util.Arrays;
+import java.util.Map;
 import java.util.Properties;
 
 /**
@@ -20,13 +19,14 @@ import java.util.Properties;
 public class MySimpleConsumer {
     private final static String TOPIC_NAME = "my-simple-topic";
     private final static String CONSUMER_GROUP_NAME = "testGroup";
+
     public static void main(String[] args) {
         Properties props = new Properties();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.229.20:9092");
         // 消费分组名
         props.put(ConsumerConfig.GROUP_ID_CONFIG, CONSUMER_GROUP_NAME);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,  StringDeserializer.class.getName());
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
 
 //        // 是否⾃动提交offset，默认就是true
 //        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
@@ -36,7 +36,7 @@ public class MySimpleConsumer {
         props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
 
         //1.创建⼀个消费者的客户端
-        KafkaConsumer<String, String> consumer = new KafkaConsumer< >(props);
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
         //2. 消费者订阅主题列表
         consumer.subscribe(Arrays.asList(TOPIC_NAME));
         while (true) {
@@ -50,14 +50,27 @@ public class MySimpleConsumer {
                         record.partition(), record.offset(), record.key(), record.value());
             }
 
+//            //所有的消息已消费完
+//            if (records.count() > 0) {
+//                //有消息
+//                // ⼿动同步提交offset，当前线程会阻塞直到offset提交成功
+//                // ⼀般使⽤同步提交，因为提交之后⼀般也没有什么逻辑代码了
+//                consumer.commitSync();//=======阻塞=== 提交成功
+//            }
+
             //所有的消息已消费完
             if (records.count() > 0) {
-                //有消息
-                // ⼿动同步提交offset，当前线程会阻塞直到offset提交成功
-                // ⼀般使⽤同步提交，因为提交之后⼀般也没有什么逻辑代码了
-                consumer.commitSync();//=======阻塞=== 提交成功
+                // ⼿动异步提交offset，当前线程提交offset不会阻塞，可以继续处理后⾯的程序逻辑
+                consumer.commitAsync(new OffsetCommitCallback() {
+                    @Override
+                    public void onComplete(Map<TopicPartition, OffsetAndMetadata> offsets, Exception exception) {
+                        if (exception != null) {
+                            System.err.println("Commit failed for " + offsets);
+                            System.err.println("Commit failed exception: " + exception.getStackTrace());
+                        }
+                    }
+                });
             }
-
         }
     }
 
